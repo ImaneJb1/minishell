a008dfd898fbba3a4f9edf2e38aa9bbb308d7d1f
ImaneJb1 <imaane@gmail.com>
2025-05-20 16:58:28 +0100

    handle -nnnnnnnnnnnnnnnnnnnnnnn case in echo.c

--- built_ins/echo/echo.c
+++ built_ins/echo/echo.c
@@ -1,22 +1,44 @@
 #include "../built_in.h"
 
-void    echo(char **args, int fd_out)
+bool    is_dash_n(char *args)
 {
     int i;
 
-    i = 0;
-    if(!args)
+    i = 1;
+    if(args[0] != '-')
+        return(FALSE);
+    while(args[i] == 'n')
+        i++;
+    if(args[i] == 0)
+        return(TRUE);
+    return(FALSE);
+}
+
+void    echo(char **args, int fd_out)
+{
+    int i;
+    int flag;
+
+    i = 1;
+    flag = 0;
+    if(!args[i])
     {
         write(fd_out, "\n", 1);
         return;
     }
     while(args[i])
     {
-        write(fd_out, &args[i],ft_strlen(args[i]));
+        if((is_dash_n(args[i]) == FALSE) || (is_dash_n(args[i]) == TRUE && flag == 1))
+        {
+            write(fd_out, &args[i],ft_strlen(args[i]));
+            i++;
+            if(args[i]) 
+                write(fd_out, " ",1);
+            flag = 1;
+        }
         i++;
-        if(args[i]) 
-            write(fd_out, " ",1);
     }
-    if(ft_strcmp(args[0], "-n") != 0)
+    if(is_dash_n(args[1]) == FALSE)
         write(fd_out, "\n", 1);
 }
+
