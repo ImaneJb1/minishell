b1cda1a0b51b5fc0f0e15d89a643c25505b5bb48
ImaneJb1 <imaane@gmail.com>
2025-06-26 17:15:40 +0100

    fix exit and empty command

--- execution/built_ins/exit/exit_func.c
+++ execution/built_ins/exit/exit_func.c
@@ -21,20 +21,25 @@ bool	is_numeric_arg(char *arg)
 void	exit_func(t_exec *node)
 {
 	if (!node->args || !node->args[1])
-		free_exit(0);
+	{
+		print_msg_to_fd("exit\n", NULL, NULL, 1);
+		free_exit(g_exit_status);
+	}
 	else if (node->args[1])
 	{
 		if (is_numeric_arg(node->args[1]))
 		{
 			if(!node->args[2])
+			{
+				write(1, "exit\n", 5);
 				free_exit(ft_atoi(node->args[1]));
+			}
 			else
 				print_msg_to_fd("exit\n", "minishell: exit: ", "too many arguments\n", 2);
 		}
 		else
 		{
-			print_msg_to_fd("exit\n", "minishell: ", "exit: ", 2);
-			print_cmd_error(node->args[1], "numeric argument required");
+			print_msg_to_fd("exit\nminishell: exit: ", node->args[1], "numeric argument required\n", 2);
 			free_exit(2);
 		}
 	}
--- execution/pipes/pipe_cmd_exec.c
+++ execution/pipes/pipe_cmd_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe_cmd_exec.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imeslaki <imeslaki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: imane <imane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/21 14:54:47 by ijoubair          #+#    #+#             */
-/*   Updated: 2025/06/26 14:37:58 by imeslaki         ###   ########.fr       */
+/*   Updated: 2025/06/26 16:53:19 by imane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	execution(t_exec *cmd)
 {
+	if(cmd->cmd == NULL)
+		free_exit(0);
 	builtin(cmd);
 	if (ft_strchr(cmd->cmd, '/') && opendir(cmd->cmd))
 	{
@@ -26,8 +28,6 @@ void	execution(t_exec *cmd)
 
 void	execute_first_command(t_exec *cmd, int *fd)
 {
-	// if(cmd->fdin_arr[0] < 0 || cmd->fdout_arr[0] < 0)
-	// 	ft_exit(1);
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	if (cmd->fd_in != STDIN_FILENO)
@@ -46,8 +46,6 @@ void	execute_first_command(t_exec *cmd, int *fd)
 
 void	execute_middle_command(t_exec *cmd, int *fd)
 {
-	// if(cmd->fdin_arr[0] < 0 || cmd->fdout_arr[0] < 0)
-	// 	ft_exit(1);
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	if (cmd->fd_in != STDIN_FILENO)
